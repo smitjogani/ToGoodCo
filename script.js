c5715e1e04992a7d5eddedb92995eac06dcca708
smit <smitjogani00@gmail.com>
2024-01-24 00:56:37 +0530

    TwoGoodCo UI

---
+++ script.js
@@ -0,0 +1,53 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+function videoconAnimation() {
+    var videocon = document.querySelector("#video-container")
+    var playbtn = document.querySelector("#play")
+
+    videocon.addEventListener("mouseenter", function () {
+        // playbtn.style.opacity = 1
+        // playbtn.style.scale = 1
+        gsap.to(playbtn, {
+            scale: 1,
+            opacity: 1
+        })
+    })
+
+    videocon.addEventListener("mouseleave", function () {
+        // playbtn.style.opacity = 1
+        // playbtn.style.scale = 1
+        gsap.to(playbtn, {
+            scale: 0,
+            opacity: 0
+        })
+    })
+
+    videocon.addEventListener("mousemove", function (dets) {
+        gsap.to(playbtn, {
+            left: dets.x - 50,
+            top: dets.y - 40
+        })
+    })
+}
+videoconAnimation()
+
+function lodingAnimation() {
+    gsap.from("#page1 h1", {
+        y: 100,
+        opacity: 0,
+        delay: 0.5,
+        duration: 0.5,
+        stagger: 0.3
+    })
+
+    gsap.from("#page1 #video-container", {
+        scale: 0.9,
+        opacity: 0,
+        delay: 1.3,
+        duration: 0.5,
+    })
+}
+lodingAnimation()
